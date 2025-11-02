de0cb6021c4933be474f663786d132e6ea0622b9
GeekHirusha Dev <geekhirusha@gmail.com>
2025-11-02 09:44:02 +0530

    Update index.html cache revision in service worker
    Changed the revision hash for index.html in the service worker's precache manifest to ensure clients receive the latest version.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-418c2b01'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7lleo5ldn98"
+    "revision": "0.25t7c4apalo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
